e3af34cf943ff6f965c8e13666d0aee0a43c1e93
RubenMarukyan <marukyan.ruben@gmail.com>
2017-03-10 16:59:56 +0400

    added  Forgotten Start Activity and LoginFragment

---
+++ app/src/main/java/com/team_red/melody/StartActivityFragments/LoginFragment.java
@@ -0,0 +1,34 @@
+package com.team_red.melody.StartActivityFragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.team_red.melody.R;
+
+
+public class LoginFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.login_fragment, container , false);
+        return view;
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
