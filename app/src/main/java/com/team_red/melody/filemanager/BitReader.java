d60070304fc5a777c9bd676827a06041ff905580
keshishyanv <keshishyanv@gmail.com>
2017-03-23 16:27:53 +0400

    some exporting fixes

---
+++ app/src/main/java/com/team_red/melody/filemanager/BitReader.java
@@ -0,0 +1,28 @@
+package com.team_red.melody.filemanager;
+
+/**
+ * Created by Havayi on 23-Mar-17.
+ */
+
+public class BitReader {
+
+    public static boolean[] toBitBool(byte newByte){
+
+        // get the value of hte byte
+
+
+        // make a bool array for the bits.
+        // 0 == falseu
+        // 1 == true; duh
+        boolean[] myByte = new boolean[8];
+        for (int i = 0; i < 8; i++)
+        {
+            // ANDing against each bit to set the bool value
+            if ((newByte & (1 << (7 - i))) != 0)
+            {
+                myByte[i] = true;
+            }
+        }
+        return myByte;
+    }
+}
