efa05dddc440711cb2dea642969731cbe116630f
TathevM <tatev.matevosyan@gmail.com>
2017-04-08 18:38:14 +0400

    Icons, EditText's

--- app/src/main/java/com/team_red/melody/activities/StartActivity.java
+++ app/src/main/java/com/team_red/melody/activities/StartActivity.java
@@ -1,14 +1,19 @@
 package com.team_red.melody.activities;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 
 import com.team_red.melody.R;
 import com.team_red.melody.StartActivityFragments.LoginFragment;
 import com.team_red.melody.StartActivityFragments.UsersOrCompsListFragment;
+import com.team_red.melody.widget.MelodyEditText;
+
+import static com.team_red.melody.models.MelodyStatics.MAIN_FONT_NAME;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -24,6 +29,10 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        MelodyEditText pageLabel = (MelodyEditText) findViewById(R.id.page_label);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), MAIN_FONT_NAME);
+        pageLabel.setTypeface(typeface);
+        pageLabel.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);
         initFragments();
     }
 
